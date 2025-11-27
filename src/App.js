83e204593f7871dbc251f6d4b315bf65ffe70a72
Leonardo Jose Amaris Dominguez <leonardo.amaris@udea.edu.co>
2025-11-26 21:28:31 -0500

    terminacion de lab 1, arquisoft

--- src/App.js
+++ src/App.js
@@ -10,15 +10,35 @@ function App() {
   const [selectedCustomerId] = useState(null);
 
   return (
-    <div style={{ padding: 20 }}>
+    <div className="app-root">
       <div className="top-banner">
-        <h1>Banco UdeA</h1>
+        <h1>Banco de la UdeA</h1>
       </div>
-      <AddCustomerForm />
-      <TransferForm />
-      <TransactionList />
-      <CustomerDetail id={selectedCustomerId} />
-      <CustomerManager />
+
+      <div className="container">
+        <div className="grid">
+          <div className="column">
+            <div className="box">
+              <h2 className="box-title">Clientes</h2>
+              <AddCustomerForm />
+            </div>
+
+            <div className="box">
+              <CustomerManager />
+            </div>    
+          </div>
+
+          <div className="column">
+            <div className="box">
+              <h2 className="box-title">Transferencias</h2>
+              <TransferForm />
+            </div>
+            <div className="box">
+              <TransactionList /> 
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
